51b1b2eb113527abcc92d66d5d9cdc99a9d11495
Rudolfs-Javalds <rudolfsjavalds@gmail.com>
2024-03-18 10:26:22 +0200

    Pievienoju iesākumu maksimas cenu meklētājam

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def rimi_cena(ko_mekle, kategorija):
     URL = "https://www.rimi.lv/e-veikals/lv/meklesana?currentPage=1&pageSize=20&query=" + ko_mekle + "%3Apriceunit-asc%3AassortmentStatus%3AinAssortment%3AallCategories%3A" + kategorija
     lapa_1 = requests.get(URL)
     zupa_1 = BeautifulSoup(lapa_1.content, "html.parser")
+    # print(zupa_1.prettify())
     rez_1 = json.loads(str(zupa_1.find("div", {"class":"js-product-container card -horizontal-for-mobile"}).get("data-gtm-eec-product"))) #Atrod produkta info
     # print(str(ko_mekle).lower(), str(rez_1['name']).lower(), (str(ko_mekle).lower() in str(rez_1['name']).lower()))
     while str(ko_mekle).lower() not in str(rez_1['name']).lower(): # Ja tā nosaukumā nav ko meklē
@@ -41,7 +42,21 @@ def rimi_cena(ko_mekle, kategorija):
         return float(rez_sarakste[1].strip().replace(',', '.')), rez_sarakste[2].strip(), saite
     except:
         return 0, ""
+    
 
+def maxima_cena(ko_mekle, kategorija):
+    URL = f"https://www.barbora.lv/meklet?order=priceAsc&q={str(ko_mekle).lower()}"
+    lapa_1 = requests.get(URL)
+    zupa_1 = BeautifulSoup(lapa_1.content, "html.parser")
+    # print(zupa_1)
+    cik=0
+    for n in zupa_1.find_all('script'):
+        cik+=1
+        if cik==21: # 22. pēc kārtas ir tieši tas info, ko man vajag / pēc kā var turpināt meklēt
+            print(n)
+
+
+maxima_cena("burk%25C4%2581ni", "dārzeņi")
 window = sg.Window('Projekts', layout)
 while True:
     event, values = window.read()
