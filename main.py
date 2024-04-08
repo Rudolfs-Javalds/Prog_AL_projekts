afee812aaf72a4d21efd3189aafb677ee0897073
Rudolfs-Javalds <rudolfsjavalds@gmail.com>
2024-04-08 10:31:15 +0300

    Gandrīz pabeigta maxima

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ import ast
 kas_janomaina = ['ā', 'č', 'ē', 'ģ', 'ī', 'ķ', 'ļ', 'ņ', 'š', 'ū', 'ž', ' ', '.', ',', '+']
 uz_ko_janomaina = ['a', 'c', 'e', 'g', 'i', 'k', 'l', 'n', 's', 'u', 'z', '-', '', '', '']
 kategorijas_rimi = {"Augļi":"SH-2-1", "Ogas":"SH-2-1", "Dārzeņi":"SH-2-2"}
+kategorijas_maxima = {"Augļi":"Augļi un dārzeņi/Augļi un ogas", "Ogas":";Augļi un dārzeņi/Augļi un ogas", "Dārzeņi":"Augļi un dārzeņi/Dārzeņi"}
 kategorijas = ["Augļi", "Ogas", "Dārzeņi"]
 lst = sg.Combo(kategorijas, enable_events=True, key='-COMBO-', readonly=True)
 
 layout = [ [sg.Text("Lūdzu, ievadiet kādu preci meklējat: "), sg.Input(key='-INPUT-', enable_events=True)],
            [sg.Text("Iezvēlieties šīs preces kategoriju: "), lst],# sg.Combo(kategorijas, key='-COMBO-', enable_events=True, readonly=True)]
            [sg.Button("Meklēt")],
-           [sg.Text("Lētāka prece Rimi veikalā: "), sg.Text("", key='-OUTPUT_RIMI-')],
-           [sg.Text("Hipersaite uz preci Rimi veikalā: "), sg.InputText("", key='-SAITE_RIMI-', enable_events=True, font=("Arial", 11, "underline"), readonly=True)]]
+           [sg.Text("Lētākā prece Rimi veikalā: "), sg.Text("", key='-OUTPUT_RIMI-')],
+           [sg.Text("Lētākā prece Maxima veikalā: "), sg.Text("", key='-OUTPUT_MAXIMA-')],
+           [sg.Text("Hipersaite uz preci Rimi veikalā: "), sg.InputText("", key='-SAITE_RIMI-', enable_events=True, font=("Arial", 11, "underline"), readonly=True)],
+           [sg.Text("Hipersaite uz preci Maxima veikalā: "), sg.InputText("", key='-SAITE_MAXIMA-', enable_events=True, font=("Arial", 11, "underline"), readonly=True)]]
 
 def rimi_cena(ko_mekle, kategorija):
     URL = "https://www.rimi.lv/e-veikals/lv/meklesana?currentPage=1&pageSize=20&query=" + ko_mekle + "%3Apriceunit-asc%3AassortmentStatus%3AinAssortment%3AallCategories%3A" + kategorija
@@ -47,59 +50,54 @@ def rimi_cena(ko_mekle, kategorija):
 
 def maxima_cena(ko_mekle, kategorija):
     URL = f"https://www.barbora.lv/meklet?order=priceAsc&q={str(ko_mekle).lower()}"
-    # print(URL)
+    print(URL)
     lapa_1 = requests.get(URL)
     zupa_1 = BeautifulSoup(lapa_1.content, "html.parser")
-    # print(zupa_1)
     cik=0
 
     for n in zupa_1.find_all('script'):
         cik+=1
-        # print(zupa_1)
         if cik==21: # 22. pēc kārtas ir tieši tas info, ko man vajag / pēc kā var turpināt meklēt
-            # print(n)
-            # print(type(n))
             n = str(n).replace('window.b_productList = ', '').replace("<script>", '').replace("</script>", '').replace("false", '"false"').replace("true", '"true"').replace("null", '"null"').replace("{[", '{').replace("]}", '}').replace("[}", "[]}").strip()
-            # print(n, '\n', len(n))
-            n_dumps = json.dumps(n)
-            n_loads=json.loads(n_dumps)
-            # teksts_load=ast.literal_eval(n_dumps)
-            # print(type(n_loads))
-            # print(type(teksts_load))
-            f = open('dati_1.json', 'w', encoding="utf-8")
-            f.write(n.replace(',', ',\n'))
-            f.close()
-            # dump = f.read()
-            f = open('dati_1.json', 'r', encoding="utf-8")
+            n=n.replace(',', ',\n')
+            with open('dati_maxima.txt', 'w', encoding='utf-8') as txt_f:
+                txt_f.write(n)
+            with open('dati_maxima.txt', 'r+', encoding='utf-8') as txt_f:
+                linijas = txt_f.readlines()
+                with open('dati_maxima.json', 'w', encoding='utf-8') as json_f:
+                    for k in range(len(linijas)):
+                        # print (k, linijas)
+                        # print (k, len(linijas))
+                        if k!=0 and (linijas[k][0]!='[' and linijas[k][0]!='{' and linijas[k][0]!='"'):
+                            linijas[k-1]=linijas[k-1].replace('\n', '')
+                            # print(linijas[k])
+                        if k!=0:
+                            json_f.write(linijas[k-1])
+                with open('dati_maxima.json', 'a', encoding='utf-8') as json_f:
+                    json_f.write(linijas[-1].replace(';', ''))
+                    
+            f = open('dati_maxima.json', 'r', encoding="utf-8")
             data = json.load(f)
             f.close()
-            print(type(data))
-            # print(n_loads[-9],n_loads[-8], n_loads[-7], n_loads[-6],n_loads[-5],n_loads[-4],n_loads[-3],n_loads[-2],n_loads[-1], sep='')
+            # print(type(data))
+            for produkts in data:
+                if kategorija.lower() in produkts["category_name_full_path"].lower():
+                    print(produkts)
+                    break
+
+            
 
 
-            # n_list = str(n).split('{"attributes":{"list":[]}')
-            # print(n_list[1])
-            # for k in n_list:
-            #     if k!=n_list[-1] and k!=n_list[0]:
-            #         k='{' + k.strip(',')
-            #         k=k.rstrip(';')
-            #         teksts = k.replace('false', '"False"').replace('true', '"True"').replace('null', '0').replace('<script>', '').replace('window.b_productList = [', '')
-            #         teksts_load=ast.literal_eval(teksts) 
-            #         print(teksts_load, '\n', type(teksts_load), '\n')
-            #         print(teksts_load.keys())
-            #         # if kategorija in teksts_load['category_name_full_path']:
-            #         #     print(teksts_load["picking_actions"])
-
-
-maxima_cena("burk%25C4%2581ni", "dārzeņi")
+maxima_cena("burk%25C4%2581ni", "Augļi un dārzeņi/Dārzeņi")
 window = sg.Window('Projekts', layout)
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
     if event == 'Meklēt':
-        kategorijas_id=kategorijas_rimi[values['-COMBO-']]
-        cena_rimi, mervien_rimi, hipersaite_rimi = rimi_cena(values['-INPUT-'], kategorijas_id)
+        kategorijas_rimi_id=kategorijas_rimi[values['-COMBO-']]
+        kategorijas_maxima_id=kategorijas_maxima[values['-COMBO-']]
+        cena_rimi, mervien_rimi, hipersaite_rimi = rimi_cena(values['-INPUT-'], kategorijas_rimi_id)
         if cena_rimi == 0:
             window['-OUTPUT_RIMI-'].update(value="Diemžēl šāds produkts nav pieejams")
         else:
